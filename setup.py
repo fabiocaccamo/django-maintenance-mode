08a3a8e679008b87f6a2e0310e74666387823034
Fabio Caccamo <fabio.caccamo@gmail.com>
2015-06-08 15:15:12 +0200

    Added setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+exec(open('maintenance_mode/version.py').read())
+
+setup(
+    name='django-maintenance-mode',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    version=__version__,
+    description='django-maintenance-mode shows a 503 error page when maintenance-mode is on.',
+    author='Fabio Caccamo',
+    author_email='fabio.caccamo@gmail.com',
+    url='https://github.com/fabiocaccamo/django-maintenance-mode',
+    download_url='https://github.com/fabiocaccamo/django-maintenance-mode/archive/%s.tar.gz' % __version__,
+    keywords = ['django', 'maintenance', 'mode', 'offline', 'under', '503', 'service', 'temporarily', 'unavailable'],
+    requires=['django(>=1.4)'],
+    classifiers=[]
+)
+
