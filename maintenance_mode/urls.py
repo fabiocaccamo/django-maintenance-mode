2c458d16d97a7c657054e7a584c15a074d810d9b
Fabio Caccamo <fabio.caccamo@gmail.com>
2016-08-30 10:38:15 +0200

    Fixed django.conf.urls.patterns warning

--- maintenance_mode/urls.py
+++ maintenance_mode/urls.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from maintenance_mode import views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^off/$', views.maintenance_mode_off, name='maintenance_mode_off'),
     url(r'^on/$', views.maintenance_mode_on, name='maintenance_mode_on'),
-)
+]
 
