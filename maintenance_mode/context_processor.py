a8a5f35c85e17b4f6cda72ace3868ebdeb075234
elmhaidara <elmhaidara@gmail.com>
2016-09-09 17:58:50 +0200

    update comments

--- maintenance_mode/context_processor.py
+++ maintenance_mode/context_processor.py
@@ -3,8 +3,11 @@ from maintenance_mode.core import get_maintenance_mode
 
 def maintenance_mode_context_processor(request):
     """
-    This function passes maintenance mode status to each view rendered with a RequestContext.
+    This function passes maintenance mode status to each view rendered with a RequestContext. It's used as context
+    processor.
+    https://docs.djangoproject.com/en/1.9/ref/templates/api/#writing-your-own-context-processors
+    https://docs.djangoproject.com/en/1.9/ref/templates/api/#django.template.RequestContext
     :param request:
-    :return:
+    :return: maintenance mode status
     """
     return {'maintenance_mode': get_maintenance_mode()}
