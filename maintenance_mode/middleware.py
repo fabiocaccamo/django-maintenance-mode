38cf314850592a9be45d61283378b725d96b3247
Fabio Caccamo <fabio.caccamo@gmail.com>
2017-02-13 10:25:17 +0100

    Fixed #16 - django 1.10 middleware compliant

--- maintenance_mode/middleware.py
+++ maintenance_mode/middleware.py
@@ -11,11 +11,18 @@ if django.VERSION < (1, 8):
 
 from django.utils.cache import add_never_cache_headers
 
+if django.VERSION < (1, 10):
+    __MaintenanceModeMiddlewareBaseClass = object
+else:
+    #https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
+    from django.utils.deprecation import MiddlewareMixin
+    __MaintenanceModeMiddlewareBaseClass = MiddlewareMixin
+
 from maintenance_mode import core
 from maintenance_mode import settings
 
 
-class MaintenanceModeMiddleware(object):
+class MaintenanceModeMiddleware(__MaintenanceModeMiddlewareBaseClass):
 
     def process_request(self, request):
 
