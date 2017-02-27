5d4838045209c7c48d0dacc155fc0765bfa2f4d6
Fabio Caccamo <fabio.caccamo@gmail.com>
2017-02-27 10:57:51 +0100

    Changed import statement

--- maintenance_mode/context_processors.py
+++ maintenance_mode/context_processors.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from maintenance_mode.core import get_maintenance_mode
+from maintenance_mode import core
 
 
 def maintenance_mode(request):
-    return { 'maintenance_mode':get_maintenance_mode() }
+    return { 'maintenance_mode':core.get_maintenance_mode() }
 
