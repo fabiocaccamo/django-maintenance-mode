2d84bb18208cf57a800d1c918f7aa5b144f881cf
Fabio Caccamo <fabio.caccamo@gmail.com>
2017-02-27 10:58:40 +0100

    Added django 1.10 commands compatibility

--- maintenance_mode/management/commands/maintenance_mode.py
+++ maintenance_mode/management/commands/maintenance_mode.py
@@ -2,30 +2,41 @@
 
 from __future__ import absolute_import
 
+import django
+
 from django.core.management.base import BaseCommand
 
 from maintenance_mode import core
 
 
 class Command(BaseCommand):
-    
+
     args = '<on|off>'
     help = 'run python manage.py maintenance_mode %s to change maintenance-mode state' % args
-    
+
+    def add_arguments(self, parser):
+
+        parser.add_argument('state')
+
     def handle(self, *args, **options):
-        
-        if len(args) > 0:
-            
-            arg_value = args[0].lower()
-            
-            if arg_value in ['on', 'yes', 'true', '1']:
-                core.set_maintenance_mode(True)
+
+        if django.VERSION < (1, 8):
+
+            if len(args) != 1:
+                print(self.help)
                 return
-                
-            elif arg_value in ['off', 'no', 'false', '0']:
-                core.set_maintenance_mode(False)
-                return
-                
-        print(self.help)
-        
-        
+
+            state = args[0]
+        else:
+            state = options['state']
+
+        state = state.lower()
+
+        if state in ['on', 'yes', 'true', '1']:
+            core.set_maintenance_mode(True)
+
+        elif state in ['off', 'no', 'false', '0']:
+            core.set_maintenance_mode(False)
+        else:
+            print(self.help)
+
