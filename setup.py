cb552869abcf6f4ff0f97e43512d124546335842
Sergio Rus <sergio@sergiorus.com>
2017-07-31 14:10:14 +0100

    Use default URLs

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     description='django-maintenance-mode shows a 503 error page when maintenance-mode is on.',
     author='Fabio Caccamo',
     author_email='fabio.caccamo@gmail.com',
-    url='https://github.com/WhoSampled/django-maintenance-mode',
-    download_url='https://github.com/WhoSampled/django-maintenance-mode/archive/%s.tar.gz' % __version__,
+    url='https://github.com/fabiocaccamo/django-maintenance-mode',
+    download_url='https://github.com/fabiocaccamo/django-maintenance-mode/archive/%s.tar.gz' % __version__,
     keywords = ['django', 'maintenance', 'mode', 'offline', 'under', '503', 'service', 'temporarily', 'unavailable'],
     requires=['django(>=1.7)'],
     classifiers=[
