b169f57c55a59e4826e7f8e4dec0cb1be0012ea0
arrahimi2 <43467327+arrahimi2@users.noreply.github.com>
2018-10-23 15:39:38 +0200

    Update context_processors.py

--- maintenance_mode/context_processors.py
+++ maintenance_mode/context_processors.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from maintenance_mode.core import get_maintenance_mode
+from maintenance_mode.core import get_maintenance_mode, get_maintenance_mode_up_time
 
 
 def maintenance_mode(request):
-    return { 'maintenance_mode':get_maintenance_mode() }
+    return { 'maintenance_mode':get_maintenance_mode(),
+             'maintenance_mode_up_time': get_maintenance_mode_up_time() }
