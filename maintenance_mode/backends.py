b123b3e704728eb053ba417cfc0d7eba72163c99
Mark Walker <theshow@gmail.com>
2019-05-15 16:00:04 +0100

    Added an S3 file backend
    If running a system with multiple servers you need a shared place to maintain state. S3 is perfect for this because as part of maintenance you may be modifying your cache or database so S3 could maintain state throughout such changes.

--- maintenance_mode/backends.py
+++ maintenance_mode/backends.py
@@ -2,6 +2,9 @@
 
 from django.conf import settings
 
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
 from maintenance_mode.io import read_file, write_file
 
 
@@ -29,3 +32,21 @@ class LocalFileBackend(AbstractStateBackend):
             raise ValueError('state file content value is not 0|1')
         write_file(settings.MAINTENANCE_MODE_STATE_FILE_PATH, value)
 
+
+class S3FileBackend(AbstractStateBackend):
+
+    def get_value(self):
+        try:
+            value = default_storage.open(settings.MAINTENANCE_MODE_STATE_FILE_NAME).read()
+        except IOError:
+            return False
+        if value not in ['0', '1']:
+            raise ValueError('state file content value is not 0|1')
+        value = bool(int(value))
+        return value
+
+    def set_value(self, value):
+        value = str(int(value))
+        if value not in ['0', '1']:
+            raise ValueError('state file content value is not 0|1')
+        default_storage.save(settings.MAINTENANCE_MODE_STATE_FILE_NAME, ContentFile(value))
