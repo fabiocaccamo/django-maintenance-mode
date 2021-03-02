e2e133157712921c65a0f9cf9e922e42454c05e3
Tolga Üstünkök <tolgaustunkok@gmail.com>
2021-03-02 16:52:55 +0300

    Redirect routes are converted to a KeyError free approach.

--- maintenance_mode/views.py
+++ maintenance_mode/views.py
@@ -13,7 +13,7 @@ def maintenance_mode_off(request):
     if request.user.is_superuser:
         set_maintenance_mode(False)
 
-    return HttpResponseRedirect(request.META['SCRIPT_NAME'] or '/')
+    return HttpResponseRedirect(request.META.get('SCRIPT_NAME', '/'))
 
 
 def maintenance_mode_on(request):
@@ -24,4 +24,4 @@ def maintenance_mode_on(request):
     if request.user.is_superuser:
         set_maintenance_mode(True)
 
-    return HttpResponseRedirect(request.META['SCRIPT_NAME'] or '/')
+    return HttpResponseRedirect(request.META.get('SCRIPT_NAME', '/'))
