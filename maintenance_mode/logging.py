261bc334f313ccfa1bba5fd08118884eef256eaa
Fabio Caccamo <fabio.caccamo@gmail.com>
2021-09-24 09:15:17 +0200

    Fixed logging filter condition to disable emailing 503 errors to admins while maintenance mode is enabled. #72

--- maintenance_mode/logging.py
+++ maintenance_mode/logging.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 
 from django.conf import settings
 
+from maintenance_mode.core import get_maintenance_mode
+
 import logging
 
 
@@ -18,6 +20,6 @@ class RequireNotMaintenanceMode503(logging.Filter):
         the given record has a status code of 503.
         """
         status_code = getattr(record, 'status_code', None)
-        if settings.MAINTENANCE_MODE and status_code == 503:
+        if get_maintenance_mode() and status_code == 503:
             return False
         return True
