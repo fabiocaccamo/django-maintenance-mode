625c67d222c6d76caa3629d9ee562966fa1d649a
Fabio Caccamo <fabio.caccamo@gmail.com>
2021-12-08 16:24:32 +0100

    Updated middleware for django 4.0 compatibility.

--- maintenance_mode/middleware.py
+++ maintenance_mode/middleware.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import django
-
-if django.VERSION < (1, 10):
-    __MaintenanceModeMiddlewareBaseClass = object
-else:
-    # https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
-    from django.utils.deprecation import MiddlewareMixin
-    __MaintenanceModeMiddlewareBaseClass = MiddlewareMixin
-
 from maintenance_mode.http import (
     get_maintenance_response, need_maintenance_response, )
 
 
-class MaintenanceModeMiddleware(__MaintenanceModeMiddlewareBaseClass):
+class MaintenanceModeMiddleware(object):
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.process_request(request)
+        if response is None and callable(self.get_response):
+            response = self.get_response(request)
+        return response
 
     def process_request(self, request):
         if need_maintenance_response(request):
             return get_maintenance_response(request)
-        else:
-            return None
+        return None
