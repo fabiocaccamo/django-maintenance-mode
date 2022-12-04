95c82bf7837fdfdee6c17191830425dd0d38ad80
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-12-04 14:45:58 +0100

    Fix tests settings.

--- tests/settings.py
+++ tests/settings.py
@@ -38,6 +38,6 @@ TEMPLATES = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": ":memory:",
     }
 }
