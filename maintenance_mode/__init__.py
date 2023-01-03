8f55bb040411c33a3de5648b1f2ca3ef9223eea0
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-01-03 12:20:45 +0100

    Fix `settings` not configured yet when reading version attr from `setup.cfg`.

--- maintenance_mode/__init__.py
+++ maintenance_mode/__init__.py
@@ -1,1 +1,6 @@
-from maintenance_mode import settings
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    from maintenance_mode import settings
+except ImproperlyConfigured:
+    pass
