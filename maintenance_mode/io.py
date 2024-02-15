e67a40b082f6e50440f78545691473f924328349
Fabio Caccamo <fabio.caccamo@gmail.com>
2024-02-15 18:41:53 +0100

    Sets the file permissions by inheriting them from the directory that contains it.

--- maintenance_mode/io.py
+++ maintenance_mode/io.py
@@ -1,20 +1,31 @@
 import fsutil
 
 
-def read_file(file_path, default_content=""):
+def read_file(filepath, default_content=""):
     """
     Read file at the specified path.
     If file doesn't exist, it will be created with default-content.
     Returns the file content.
     """
-    if not fsutil.exists(file_path):
-        fsutil.write_file(file_path, default_content, atomic=True)
-    return fsutil.read_file(file_path) or default_content
+    if not fsutil.exists(filepath):
+        write_file(filepath, default_content)
+    return fsutil.read_file(filepath) or default_content
 
 
-def write_file(file_path, content):
+def set_file_permissions(filepath):
+    """
+    Sets the file permissions by inheriting
+    them from the directory that contains it.
+    """
+    dirpath, filename = fsutil.split_filepath(filepath)
+    dirpath_permissions = fsutil.get_permissions(dirpath)
+    fsutil.set_permissions(filepath, dirpath_permissions)
+
+
+def write_file(filepath, content):
     """
     Write file at the specified path with content.
     If file exists, it will be overwritten.
     """
-    fsutil.write_file(file_path, content, atomic=True)
+    fsutil.write_file(filepath, content, atomic=True)
+    set_file_permissions(filepath)
